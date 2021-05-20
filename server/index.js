48640ff4faa6523c2251937976a8543f640579a1
Ben Lambert <kalstarn@gmail.com>
2021-05-19 23:00:21 -0600

    all fields dynamic and updateable. creating server interface 2

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./client/dist'));
 
+app.get(/)
 
 app.listen(port, () => {
   console.log(`Record Manager listening on port ${port}!`)
